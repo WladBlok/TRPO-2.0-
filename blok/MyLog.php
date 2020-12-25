b6ba9f89b4e635238d7de24ba0eff9c0ca59ccff
WladBlok <vl-blok@hotmail.com>
2020-12-25 22:58:38 +0300

    lab 9 fix

--- blok/MyLog.php
+++ blok/MyLog.php
@@ -5,13 +5,12 @@ namespace blok;
 use core\LogInterface;
 use core\LogAbstract;
 
-Class MyLog extends LogAbstract
+class MyLog extends LogAbstract
     implements LogInterface
 {
 
     public function _write()
     {
-        date_default_timezone_set('UTC+3');
         $q = date('d-m-Y\_H.i.s.u');
         foreach ($this->log as $f) {
 
@@ -19,13 +18,12 @@ Class MyLog extends LogAbstract
             $filename = __DIR__ . "\..\log";
 
             if (file_exists($filename)) {
-                file_put_contents(__DIR__ . "\..\log\\$q.log", trim($f."\r\n") . PHP_EOL, FILE_APPEND);
+
             } else {
                 mkdir(__DIR__ . "\..\log", 0777);
-                file_put_contents(__DIR__ . "\..\log\\$q.log", trim($f."\r\n") . PHP_EOL, FILE_APPEND);
             }
 
-
+            file_put_contents(__DIR__ . "\..\log\\$q.log", trim($f . "\r\n") . PHP_EOL, FILE_APPEND);
 
 
         }
@@ -33,7 +31,7 @@ Class MyLog extends LogAbstract
 
     public static function log($str)
     {
-        self::Instance()->log[]=$str;
+        self::Instance()->log[] = $str;
     }
 
     public static function write()
