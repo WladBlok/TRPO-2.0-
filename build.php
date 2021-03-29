b1422f97a62f09d149e8105e701952390c1a5819
WladBlok <73830984+WladBlok@users.noreply.github.com>
2021-03-29 16:24:39 +0300

    lab 10 fix 3.0

--- build.php
+++ build.php
@@ -1,5 +1,5 @@
 <?php
 $info = new SplFileInfo('../TRPO2');
 $info->getRealPath();
-shell_exec( 'cd '. $info .' && composer update' );
+shell_exec( 'cd '. $info .' && composer install' );
 shell_exec('git symbolic-ref --short -q HEAD>version');
